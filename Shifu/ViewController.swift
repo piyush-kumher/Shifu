1da841a0a450001863d3821fcff8be9a28e22fc4
Shubham Kankaria <shubham.kankaria@flipkart.com>
2017-06-22 15:14:55 +0530

    Intial project setup

---
+++ Shifu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shifu
+//
+//  Created by Shubham Kankaria on 22/06/17.
+//  Copyright (c) 2017 FSociety. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
